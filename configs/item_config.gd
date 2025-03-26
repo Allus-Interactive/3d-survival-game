4232b26330e1f93fbf0e6327b7614df14039ed22
Reece <reecemorgandev@gmail.com>
2025-03-26 09:28:31 +0000

    set up for item resources

--- configs/item_config.gd
+++ configs/item_config.gd
@@ -1,7 +1,16 @@
 class_name ItemConfig
 
 enum Keys {
+	Plant,
 	Stick,
-	Stone,
-	Plant
+	Stone
 }
+
+const ITEM_RESOURCE_PATHS = {
+	Keys.Plant : "res://resources/item_resources/plant_resource.tres",
+	Keys.Stick : "res://resources/item_resources/stick_resource.tres",
+	Keys.Stone : "res://resources/item_resources/stone_resource.tres"
+}
+
+static func get_item_resource(key : Keys) -> ItemResource:
+	return load(ITEM_RESOURCE_PATHS.get(key))
