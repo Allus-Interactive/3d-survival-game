757c381fe62061a72587621fe8db5609b3a47236
Reece <reecemorgandev@gmail.com>
2025-03-26 08:52:36 +0000

    added prompts for pick up items

---
+++ global/event_system.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# Bulletin signals
+signal BUL_create_bulletin
+signal BUL_destroy_bulletin
--- player/interaction_ray_cast.gd
+++ player/interaction_ray_cast.gd
@@ -1,8 +1,20 @@
 extends RayCast3D
 
+var is_looking : bool = false
+
 func check_interaction() -> void:
 	# If raycast detects interactable and interact key is pressed, start interaction
-	if is_colliding() && Input.is_action_just_pressed("interact"):
+	if is_colliding():
 		var collider = get_collider()
-		if collider is Interactable:
+		if not collider is Interactable:
+			return
+		
+		if Input.is_action_just_pressed("interact"):
 			collider.start_interaction()
+		
+		if not is_looking:
+			is_looking = true
+			EventSystem.BUL_create_bulletin.emit(BulletinConfig.Keys.InteractionPrompt, collider.prompt)
+	elif is_looking:
+		is_looking = false
+		EventSystem.BUL_destroy_bulletin.emit(BulletinConfig.Keys.InteractionPrompt)
